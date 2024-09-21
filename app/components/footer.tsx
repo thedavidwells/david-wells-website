0e34711177928e3dc854d384d0c5615ac274fbc4
David Wells <david@thedavidwells.com>
2024-09-21 11:16:34 -0600

    Latest content updates.

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -12,7 +12,7 @@ function ArrowIcon() {
         fill="currentColor"
       />
     </svg>
-  )
+  );
 }
 
 export default function Footer() {
@@ -24,10 +24,10 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="/rss"
+            href="https://x.com/thedavidwells"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">rss</p>
+            <p className="ml-2 h-7">@thedavidwells</p>
           </a>
         </li>
         <li>
@@ -35,21 +35,21 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/vercel/next.js"
+            href="https://www.linkedin.com/in/thedavidwells/"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">linkedin</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/thedavidwells"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
-          </a>
-        </li>
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">view source</p>
           </a>
         </li>
       </ul>
@@ -57,5 +57,5 @@ export default function Footer() {
         © {new Date().getFullYear()} MIT Licensed
       </p>
     </footer>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,33 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
 
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        David Wells - A Simple Blog
+        david wells
       </h1>
-      <h2>
-        About Me
-      </h2>
-      <p className="mb-4">
-        {`I'm a B2B SaaS Sales Professional based in Boise, Idaho. 
-        I have over 10 years of experience as a Software Engineer, working at companies such as FIS and Intuit.
-        Additionally, I'm a real estate investor focused on Commercial and Self-Storage properies.
-        My passion is solving problems through the use of innovative technology.`}
+      <p className="prose">
+        I'm a 10-year software development veteran turned sales professional. I
+        work at{" "}
+        <a href="https://vercel.com" target="_blank">
+          Vercel
+        </a>
+        , where I help software developers make their websites faster.
       </p>
-      <h2>Blog Posts</h2>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
+      <br></br>
+      <p>
+        As a Software Engineer, I have worked at companiessuch as FIS and Intuit
+        - most recently as a Senior Software Engineer on the QuickBooks
+        Workforce team.
+      </p>
+      <br></br>
+      <p>
+        Additionally, I'm a real estate investor focused on Commercial and
+        Self-Storage properties, but also dabbling in short-term rentals. My
+        passions are helping people and solving problems through the use of
+        innovative technology.
+      </p>
+      <br></br>
     </section>
-  )
+  );
 }
